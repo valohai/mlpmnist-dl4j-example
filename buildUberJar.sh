db5ff3675ccd7ba2398de1d0ae403ed049362991
mani <sadhak001@gmail.com>
2019-10-19 20:19:56 +0100

    Updated the build UberJar shell script to adapt to target OS when no param is passed in

--- buildUberJar.sh
+++ buildUberJar.sh
@@ -4,7 +4,19 @@ set -e
 set -u
 set -o pipefail
 
-OSNAME=${1:-linux}
+detectOSPlatform() {
+	OSNAME=$(uname)
+
+	if [[ "${OSNAME}" = "Darwin" ]]; then
+	 	OSNAME=macosx
+	elif [[ "${OSNAME}" = "Linux" ]]; then
+		OSNAME=linux
+	fi
+
+	echo ${OSNAME}
+}
+
+OSNAME=${1:-$(detectOSPlatform)}
 
 echo "Building uber jar for target platform: ${OSNAME}"
 mvn package -Djavacpp.platform=${OSNAME}-x86_64
