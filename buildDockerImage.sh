0d7e1a6cdc49981f5507ddefa76daba629bdf705
mani <sadhak001@gmail.com>
2019-11-19 13:08:44 +0000

    Adding default docker username so that it uses to do all docker image related activities

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -4,6 +4,10 @@ set -e
 set -u
 set -o pipefail
 
+if [[ -z ${DOCKER_USER_NAME:-""} ]]; then
+  read -p "Docker username (must exist on Docker Hub): " DOCKER_USER_NAME
+fi
+
 IMAGE_NAME=${IMAGE_NAME:-dl4j-mnist-single-layer}
 IMAGE_VERSION=${IMAGE_VERSION:-$(cat version.txt)}
 DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
--- runDockerContainer.sh
+++ runDockerContainer.sh
@@ -4,6 +4,7 @@ set -e
 set -u
 set -o pipefail
 
+DOCKER_USER_NAME=${DOCKER_USER_NAME:-neomatrix369}
 IMAGE_NAME=${IMAGE_NAME:-dl4j-mnist-single-layer}
 IMAGE_VERSION=${IMAGE_VERSION:-$(cat version.txt)}
 DOCKER_FULL_TAG_NAME="${DOCKER_USER_NAME}/${IMAGE_NAME}"
